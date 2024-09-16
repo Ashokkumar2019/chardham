dab2dd36b4ae7b6418aeff5b35bda45de986a456
Ashok Kumar <v-kuash@microsoft.com>
2024-09-17 01:17:39 +0530

    Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           <Route path="/Yamunotri" element={<Yamunotri/>} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
+      </div> 
     </Router>
     
     
