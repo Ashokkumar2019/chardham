649e93b6fd5165f6dfdf8cd032804ffbdb483a04
Gaurav <39293964+gauravsgc@users.noreply.github.com>
2024-09-24 16:17:46 +0530

    Update Lastvisit.jsx

--- src/Lastvisit.jsx
+++ src/Lastvisit.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import data from './jsondata/Visit.json'
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 export default function Lastvisit() {
   return (
@@ -12,12 +11,12 @@ export default function Lastvisit() {
       {data.map((item, index) => (
         <Col key={index} md={3} sm={6} className="mb-4">
           <Card className="h-100 d-flex flex-column">
-          
+            
             <Card.Img variant="top" src={item.imagePath} alt={item.title} />
             <Card.Body>
               <Card.Title>{item.title}</Card.Title>
               <Card.Text>{item.description}</Card.Text>
-              <Link to={item.URL}>Learn More</Link> 
+              <Button variant="primary" href={item.url}>Learn More</Button>
             </Card.Body>
           </Card>
         </Col>
