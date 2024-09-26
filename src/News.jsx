e2b9aa4a0c912110e8af555ca5198ad41ca3dcc9
Gaurav <39293964+gauravsgc@users.noreply.github.com>
2024-09-26 09:51:46 +0530

    Update News.jsx

--- src/News.jsx
+++ src/News.jsx
@@ -6,8 +6,10 @@ export default function News() {
     <Row className='text-center py-5'>
       <Col>
         
-        <h3>To stay updated on news related to Char Dham</h3>
-        <p>By using these resources, you can stay informed about the latest developments, travel advisories, and news related to the Char Dham sites.</p>
+      <h3>Char Dham</h3>
+        <p>The Char Dham Yatra in Uttarakhand is indeed a profound spiritual journey that attracts countless pilgrims each year. </p>
+      <p>These four sacred sites—Badrinath, Kedarnath, Gangotri, and Yamunotri—hold immense significance in Hinduism and are nestled in the stunning landscapes of the Garhwal region.
+        </p>
       </Col>
     </Row>
   </Container>
